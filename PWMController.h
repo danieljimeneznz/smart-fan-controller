cafb151a56af481bb87dc15eda54806496b4cd69
Daniel Jimenez <danieljimenez@outlook.co.nz>
2017-09-02 13:25:48 +1200

    Initial Commit

---
+++ PWMController.h
@@ -0,0 +1,21 @@
+/*
+ * PWMController.h
+ *
+ * Created: 02-Sept-17 13:02:26
+ * ELECTENG 211 Smart Fan Project
+ * Group 4
+ */ 
+
+
+#ifndef PWMCONTROLLER_H_
+#define PWMCONTROLLER_H_
+
+#include <avr/include/stdint.h>
+
+class PWMController {
+	uint8_t speed;
+};
+
+
+
+#endif /* PWMCONTROLLER_H_ */
