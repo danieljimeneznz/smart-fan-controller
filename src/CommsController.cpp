5f1c664be04f3432fc2bb46f6366e5b45c4766ad
Shaurya-D <31538201+Shaurya-D@users.noreply.github.com>
2017-09-28 11:47:07 +1300

    edited comments removed setting Pa7 to output

--- src/CommsController.cpp
+++ src/CommsController.cpp
@@ -9,24 +9,19 @@
 #include "CommsController.h"
 
 CommsController::CommsController(uint8_t ubrr) {
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0); // Enable USART  receiver
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0); // Enable Rx and Tx
 	UBRR0H = (ubrr>>8);
 	UBRR0L = ubrr;
-	// Select 8-bit data frame, single stop bit and no parity using UCSR0C (Default values are what we want).
+	// Select 8-bit data frame, double stop bit and no parity using UCSR0C (Default values are what we want).
 	UCSR0C = (1 << USBS0); // two stop bits
-	// Set PA7 as Output.
-	DDRA |= (1<<DDA7);
 
 	// Create a new tinyJSONpp Object.
 	//json = new tinyjsonpp(false, 255);
 }
 
 void CommsController::transmit(unsigned char data) {
-		//Enable Transmit
-	// Wait for empty transmit buffer.
-	while (!(UCSR0A && (1 << UDRE0)));
+	while (!(UCSR0A && (1 << UDRE0))); // Wait for empty transmit buffer.
 	UDR0 = data;
-	// After transmitting wait for buffer to be empty before allowing next transmit.
 }
 
 void CommsController::setControllerPointers(SpeedController* speedController, PowerController* powerController, ErrorHandler* errorHandler)
