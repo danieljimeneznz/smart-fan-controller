f4d858a2f0bcdb36ef2d7a866d12ee041743afe0
Shaurya Duggal <sdug668@aucklanduni.ac.nz>
2017-10-02 17:09:51 +1300

    completed run function in commscontrollerCPP

--- src/CommsController.cpp
+++ src/CommsController.cpp
@@ -9,37 +9,59 @@
 #include "CommsController.h"
 
 CommsController::CommsController(uint8_t ubrr) {
+	UCSR0B |= (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0); // Enable Rx, Tx and Rx Complete Interrupt.
 	UBRR0H = (ubrr>>8);
 	UBRR0L = ubrr;
 	// Select 8-bit data frame, double stop bit and no parity using UCSR0C (Default values are what we want).
-	UCSR0C |= (1<<USBS0); // two stop bits
+	UCSR0C |= (1<<USBS0); // Two stop bits.
 
 	// Create a new tinyJSONpp Object.
 	json = new tinyjsonpp(false, 255);
 }
 
 void CommsController::transmit(uint8_t data) {
+	UCSR0B &= ~(1<<RXEN0); // disable Rx.
 	while (!(UCSR0A&&(1<<UDRE0))); // Wait for empty transmit buffer.
-	UDR0 = data;//sending data to TX buffer
-	while (!(UCSR0A&(1<<TXC0))); // waits for Tx buffer to be empty
+	UDR0 = data; // Sending data to TX buffer.
+	while (!(UCSR0A&(1<<TXC0))); // Waits for Tx buffer to be empty.
+	UCSR0B |= (1<<RXEN0); // Enable Rx.
 }
 
 void CommsController::run(){
 
-	if (this->jsonComplete == true){//checking if Rx is complete
+	// Checking if Rx is complete
+	if (this->jsonComplete == true){
+		
 		Value val;
-		val = json->getValue("3");//checking that Rx is meant to be for our fan
-		if (val.size > 2) {//checking if only an update is requested
+		val = json->getValue("3"); // Checking that Rx is meant to be for our fan.
+		if (val.size > 2) { // Checking if only an update is requested.
 			val = json->getValue("req", "3/spd");
 			if (val.size > 0) {
 				speedController->setFanSpeed(json->convertValueToInt(val));
 			}
 			val = json->getValue("clr", "3");
 			if (val.size > 0){
-				//clear errors
+			// Clear errors
 			}
-		//give back an update
-		json->insert("spd", "SOFT_VER", "3");
+			// Give back an update (idk how to comment the json stuff)
+			json->insert("ver", SOFT_VER, "3"); 
+			json->insert("spd", "{}", "3");
+			char* spd = static_cast<char *>(calloc(4, sizeof(char))) // Allocating memory for conversion of int to string.
+			itoa(speedController->requestedSpeed, spd, 10); // Converting requested speed value from int to string.
+			json->insert("req", spd, "3/spd");
+			itoa(speedController->currentSpeed, spd, 10); // Converting current speed value from int to string.
+			json->insert("cur", spd, "3/spd");
+			// json->insert("hello", "world", "3/pwr");
+		
+			free(spd);
+
+			// uint8_t size = json->getSize();
+			uint8_t size = 255;
+			// For loop to transmit every bit of json.
+			for (uint8_t i = 0; i <= size; ++i){
+				this->transmit(json->getChar(i));
+			}
+			this->jsonComplete = false; // Resetting json complete.
 		}
 	}
 }
