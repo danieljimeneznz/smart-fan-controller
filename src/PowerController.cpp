ea9cd29737d80779824b91a139a76676210cbc33
Shaurya-D <31538201+Shaurya-D@users.noreply.github.com>
2017-10-14 10:26:24 +1300

    ADC almost complete

--- src/PowerController.cpp
+++ src/PowerController.cpp
@@ -10,25 +10,71 @@
 
 PowerController::PowerController() {
 	/** Setup and enable ADC **/
-	ADMUXB |=	(0<<REFS2)| // Reference Selection Bits
+	ADMUXB |=	(0<<REFS2)| // Reference Selection Bits.
 				(0<<REFS1)| // AVcc;
 				(0<<REFS0);
 				 
-	ADMUXA |=	(0<<MUX5)| // Set the current ADC MUX to ADC0.
+	ADMUXA |=	(0<<MUX5)| // Set the current ADC MUX to ADC11 (Vm).
 				(0<<MUX4)|
-				(0<<MUX3)|
+				(1<<MUX3)|
 				(0<<MUX2)|
-				(0<<MUX1)|
-				(0<<MUX0);
+				(1<<MUX1)|
+				(1<<MUX0);
 
-	ADCSRA =	(0<<ADEN) | // ADC Disable
-				(0<<ADSC) | // ADC Conversion bit
-				(0<<ADIE) | // ADC Interrupt Disable
-				(1<<ADPS2)| // ADC Pre-scaler Select Bits (128)
+	ADCSRA |=	(1<<ADEN)| // ADC Enable.
+				(1<<ADSC)| // Start ADC Conversion.
+				(1<<ADATE)| // ADC Auto Trigger Enable.
+				(1<<ADPS2)| // ADC pre-scaler Select Bits (64)
 				(1<<ADPS1)|
-				(1<<ADPS0);
+				(0<<ADPS0);
+
+	ADCSRB |=	(0<<ADTS2)| // Setting Free Running Mode as Trigger selection
+				(0<<ADTS1)|
+				(0<<ADTS0);
+	return;
 }
 
-float PowerController::readVoltage(uint8_t channel) {
-	return 0.0f;
+// Do not need to set ref voltage as default is Vcc.
+// pb0 = voltage input, pb1 = current measurement.
+
+
+void PowerController::setControllerPointers(ErrorHandler* errorHandler) {
+	this->errorHandler = errorHandler;
+}
+
+uint16_t PowerController::readValue(uint8_t channel) {
+	if channel == (10){
+		ADMUXA |=	(0<<MUX5)| // Set the current ADC MUX to ADC11 (Vm).
+					(0<<MUX4)|
+					(1<<MUX3)|
+					(0<<MUX2)|
+					(1<<MUX1)|
+					(1<<MUX0);
+	}
+	if channel == (11){
+		ADMUXA |=	(0<<MUX5)| // Set the current ADC MUX to ADC10 (Im).
+					(0<<MUX4)|
+					(1<<MUX3)|
+					(0<<MUX2)|
+					(1<<MUX1)|
+					(0<<MUX0);
+	}
+	while (!(1<<ADIF)); // Waiting for conversion to be complete.
+		
+	if channel == (10){ // Reading voltage.
+		voltage = ADC;
+	}
+	if channel == (11){ // Reading current.
+		current == ADC;
+	}
+			
+	return;
+}
+// we need to set ADSC back to 1 after each conversion i am unsure where to do this.
+
+uint16_t PowerController::getPower(){
+		if (voltage > 0) && (current > 0){ // (i only intend to check if voltage and current have been updated, unsure how to do it).
+			power = voltage*current;
+			return power;
+		}
 }
